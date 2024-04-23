94c59e53914d3ee12ebec8a1f41c379719fbda16
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-04-23 18:48:39 +0700

    void input bangun datar

--- praktikumIntOOP2/praktikumIntOOP2.cpp
+++ praktikumIntOOP2/praktikumIntOOP2.cpp
@@ -4,7 +4,15 @@ using namespace std;
 class bangunDatar {
 
 	//akses modifier
-private:
-	float panjang, lebar;
-public:
-	float luas;
+	private:
+		float panjang, lebar;
+	public:
+		float luas;
+
+		void input() {
+
+			cout << "Masukan Panjangnya = ";
+			cin >> panjang;
+			cout << "Masukan Lebarnya = ";
+			cin >> lebar;
+		}
